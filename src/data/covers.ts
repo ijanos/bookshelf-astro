695beea2cb9a9ad6716231bf3577657cd3e03411
János Illés <ijanos@gmail.com>
2024-03-09 17:00:10 +0100

    clean up some of the stringyness

--- src/data/covers.ts
+++ src/data/covers.ts
@@ -2,22 +2,24 @@ import path from "node:path";
 import type { ImageMetadata } from "astro";
 import bookshelf from "../data/bookshelf.json";
 
+function idToString([source, id]: [string, string]) : string {
+    return `${source}__${id}__`.toLowerCase();
+}
 
-// science has gone too far...
-const ids = new Set(bookshelf.books.flatMap((book) => book.id ? Object.entries(book.id).map(([source, id]) => `${source}__${id}__`.toLowerCase()) : []));
+const ids = new Set(bookshelf.books.flatMap((book) => book.id ? Object.entries(book.id).map(idToString) : []));
 
 const covers = new Map<string,() => Promise<{ default: ImageMetadata }>>();
 
 const imageFiles = import.meta.glob<{ default: ImageMetadata }>('/covers/*.{jpeg,jpg,png,gif}')
 
 for (const [pathString, imgPromise] of Object.entries(imageFiles)) {
-    const p = path.parse(pathString).name.toLowerCase();
-    if (!ids.has(p)) {
+    const fileName = path.parse(pathString).name.toLowerCase();
+    if (!ids.has(fileName)) {
         throw Error(`Found cover image ${pathString} with no matching book`);
     } else {
-        covers.set(p, imgPromise);
+        covers.set(fileName, imgPromise);
     }
 }
 
 
-export { covers };
+export { covers, idToString };
